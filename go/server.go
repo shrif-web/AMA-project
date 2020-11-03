2bed671e8a544ece1320e4698880aad9ff8ece50
Alireza Ziaee <alrz1999@gmail.com>
2020-11-03 21:04:35 +0330

    init

---
+++ go/server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type writeResponse struct {
+	LineNumber int
+	Line       string
+}
+
+type shaResponse struct {
+	Hash string
+}
+
+func main() {
+	http.HandleFunc("/sha", sha)
+	http.HandleFunc("/write", write)
+	http.ListenAndServe(":8000", nil)
+}
+
+func sha(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		fmt.Fprintf(w, "Sorry, only POST methods are supported for this path.")
+		return
+	}
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	number1, err1 := strconv.Atoi(req.FormValue("number1"))
+	number2, err2 := strconv.Atoi(req.FormValue("number2"))
+
+	if err1 != nil || err2 != nil {
+		fmt.Fprintf(w, "inputs must be number")
+		return
+	}
+
+	h := sha256.New()
+	h.Write([]byte(strconv.Itoa(number1 + number2)))
+	w.Header().Set("Content-Type", "application/json")
+	objectResponse := &shaResponse{
+		Hash: hex.EncodeToString(h.Sum(nil)),
+	}
+	jsonResponse, _ := json.Marshal(objectResponse)
+	fmt.Fprintf(w, string(jsonResponse))
+}
+
+func write(w http.ResponseWriter, req *http.Request) {
+	if !isValidRequest(w, req) {
+		return
+	}
+	lineNumber, error := strconv.Atoi(req.FormValue("lineNumber"))
+
+	if error != nil || lineNumber < 1 || lineNumber > 100 {
+		fmt.Fprintf(w, "lineNumber must be a number between 1 and 100")
+		return
+	}
+	line := readLine(lineNumber - 1)
+	objectResponse := &writeResponse{
+		Line:       line,
+		LineNumber: lineNumber}
+	jsonResponse, _ := json.Marshal(objectResponse)
+	fmt.Fprintf(w, string(jsonResponse))
+}
+
+func isValidRequest(w http.ResponseWriter, req *http.Request) bool {
+	if req.Method != "GET" {
+		fmt.Fprintf(w, "Sorry, only GET methods are supported for this path.")
+		return false
+	}
+	return true
+}
+
+func readLine(lineNumber int) string {
+	data, err := ioutil.ReadFile("../files/example.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return "File reading error"
+	}
+
+	return strings.Split(string(data), "\n")[lineNumber]
+}
+
+func writeToFile() {
+	f, _ := os.Create("../files/example.txt")
+
+	for i := 1; i <= 100; i++ {
+		f.WriteString(strconv.Itoa(i) + "\n")
+	}
+
+	f.Sync()
+}
---
+++ node/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+app.use(cors())
+const port = 9000
+app.use(express.urlencoded());
+app.use(express.json());
+var crypto = require('crypto');
+const lineReader = require('line-reader');
+
+app.post('/sha', (req, res) => {
+    var num1 = req.body.number1;
+    var num2 = req.body.number2;
+    if (num1 == undefined || num2 == undefined || num1 == '' || num2 == '' || Number.isNaN(num1) || Number.isNaN(num2)) {
+        res.status(400).send("inputs must be number");
+    }
+    var seed = (parseInt(num1) + parseInt(num2)).toString();
+    console.log(seed)
+    var hash = crypto.createHash('sha256').update(seed).digest('hex');
+    res.status(200).json({ Hash: hash })
+})
+
+app.get('/write', (req, res) => {
+    var lineNumber = req.query.lineNumber;
+
+    if (lineNumber == undefined || lineNumber == '' || Number.isNaN(lineNumber)) {
+        res.status(400).send("input must be number");
+    }
+
+    if (parseInt(lineNumber) < 1 || parseInt(lineNumber) > 100) {
+        res.status(400).send("lineNumber must be a number between 1 and 100");
+    }
+
+    var counter = 1
+    lineReader.eachLine('../files/example.txt', function (line) {
+        if (counter == parseInt(lineNumber)) {
+            res.status(200).send({ Line: line, LineNumber: lineNumber })
+            return false;
+        }
+        counter++;
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
