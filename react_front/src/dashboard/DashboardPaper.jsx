6b3dd67df7326ff077d1f8435d726d7e55bb44fc
abolfazl <asad.abolfazl.asad@gmail.com>
2020-11-04 03:45:14 +0330

    update fourohfour page

--- react_front/src/dashboard/DashboardPaper.jsx
+++ react_front/src/dashboard/DashboardPaper.jsx
@@ -53,7 +53,11 @@ export default function DashboardPaper() {
                   <Write serverName="node" />
                 </Route>
                 <Route path="/">
-                  <div>404</div>
+                  <h3>404 page not found</h3>
+                  <p>
+                    We are sorry but the page you are looking for does not
+                    exist.
+                  </p>
                 </Route>
               </Switch>
             </Paper>
