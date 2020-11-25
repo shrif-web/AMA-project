c0a3df992cfbc1af35754a27b8d09ec95c4dc16e
Alireza Ziaee <alrz1999@gmail.com>
2020-11-26 00:59:41 +0330

    wtf

--- HW2/src/scripts/dataTable.js
+++ HW2/src/scripts/dataTable.js
@@ -21,16 +21,16 @@ function fillCards() {
         card.classList.add("card")
         card.innerHTML = `
         <div class="card-body font-weight-bold">
-            <div class="d-flex justify-content-between">
-                <p class="card-text font-weight-light">رتبه</p>
+            <div class="d-flex justify-content-between  font-weight-light">
+                <p class="card-text">رتبه</p>
                 <p class="card-text card-data">${element.ranking}</p>
             </div>
             <div class="d-flex justify-content-between">
                 <p class="card-text">نام تیم</p>
                 <p class="card-text card-data">${element.name}</p>
             </div>
-            <div class="d-flex justify-content-between">
-                <p class="card-text font-weight-light">نام دانشگاه و کشور</p>
+            <div class="d-flex justify-content-between font-weight-light">
+                <p class="card-text">نام دانشگاه و کشور</p>
                 <p class="card-text card-data">${element.from}</p>
             </div>
             <div class="d-flex justify-content-between">
