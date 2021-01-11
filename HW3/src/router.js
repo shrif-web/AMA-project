d99bf9e282985e687ee96fdcdb95ffca3b3d46df
abolfazl <asad.abolfazl.asad@gmail.com>
2021-01-11 18:43:47 +0330

    signin added

--- HW3/src/router.js
+++ HW3/src/router.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const Post = require("./model.post");
 const User = require("./model.user");
+var jwt = require("jsonwebtoken");
 
 router
   .route("/admin/post/crud")
@@ -121,7 +122,7 @@ function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
-router.route("/signup").post(async(req, res) => {
+router.route("/signup").post(async (req, res) => {
   const { email, password } = req.body;
   if (Object.keys(req.body).length !== 2)
     return res.status(400).send({ message: "Request Length should be 2" });
@@ -132,10 +133,9 @@ router.route("/signup").post(async(req, res) => {
       .status(400)
       .send({ message: "filed `password`.length should be gt 5" });
 
-
   const user_same_email = await User.findOne({ email: email });
-  if (user_same_email){
-      return res.status(409).send({  "message": "email already exist."});
+  if (user_same_email) {
+    return res.status(409).send({ message: "email already exist." });
   }
 
   const user = new User({ email: email, password: password });
@@ -174,6 +174,42 @@ router.route("/admin/user/crud/:id").get((req, res) => {
       console.log(err);
       return res.status(500).send();
     });
+});
+
+router.route("/signin").post(async (req, res) => {
+  const { email, password } = req.body;
+  if (Object.keys(req.body).length !== 2)
+    return res.status(400).send({ message: "Request Length should be 2" });
+  if (email === undefined || !validateEmail(email))
+    return res.status(400).send({ message: "filed `email` is not valid" });
+  if (password === undefined || password.length <= 5)
+    return res
+      .status(400)
+      .send({ message: "filed `password`.length should be gt 5" });
+
+  const user_same_email = await User.findOne({ email: email });
+  if (!user_same_email) {
+    return res.status(409).send({ message: "email already exist." });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user_same_email.user_id,
+    },
+    "secret",
+    { expiresIn: "1h" }
+  );
+  console.log(token);
+  res.send({ token: token });
+});
+
+router.route("/check").get((req, res) => {
+  const token = req.body.token || "ali";
+  jwt.verify(token, "secret", (err, id) => {
+    if (err) res.status(400).send();
+    res.send(id);
+  });
+  res.send();
 });
 
 module.exports = router;
