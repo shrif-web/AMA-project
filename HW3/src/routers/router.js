2f27ccf1567d3de350e10521f500f549f80a331a
abolfazl <asad.abolfazl.asad@gmail.com>
2021-01-11 19:32:11 +0330

    token added

--- HW3/src/routers/router.js
+++ HW3/src/routers/router.js
@@ -1,14 +1,29 @@
 const { query } = require("express");
 const express = require("express");
 const router = express.Router();
-const Post = require("./model.post");
-const User = require("./model.user");
+const Post = require("../model.post");
+const User = require("../model.user");
 var jwt = require("jsonwebtoken");
+
+router.use((req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({ message: "token neded" });
+  }
+  jwt.verify(token, "secret", (err, user_id) => {
+    if (err) {
+      return res.status(403).send();
+    }
+    req.user_id = user_id.id;
+    console.log(user_id);
+    next();
+  });
+});
 
 router
   .route("/admin/post/crud")
   .get((req, res) => {
-    Post.find({})
+    Post.find({ created_by: req.user_id })
       .then((doc) => {
         return res.status(200).send({ posts: doc });
       })
@@ -27,7 +42,7 @@ router
       return res.status(400).send({ message: "filed `content` is not valid" });
 
     //TODO get user_id from token
-    const user_id = "0";
+    const user_id = req.user_id;
     const post = new Post({
       title: title,
       content: content,
@@ -117,99 +132,5 @@ router
         return res.status(500).send();
       });
   });
-
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-router.route("/signup").post(async (req, res) => {
-  const { email, password } = req.body;
-  if (Object.keys(req.body).length !== 2)
-    return res.status(400).send({ message: "Request Length should be 2" });
-  if (email === undefined || !validateEmail(email))
-    return res.status(400).send({ message: "filed `email` is not valid" });
-  if (password === undefined || password.length <= 5)
-    return res
-      .status(400)
-      .send({ message: "filed `password`.length should be gt 5" });
-
-  const user_same_email = await User.findOne({ email: email });
-  if (user_same_email) {
-    return res.status(409).send({ message: "email already exist." });
-  }
-
-  const user = new User({ email: email, password: password });
-  user
-    .save()
-    .then((data) => {
-      return res.status(201).send({ message: "user has been created." });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).send();
-    });
-});
-
-router.route("/admin/user/crud/").get((req, res) => {
-  User.find({})
-    .then((doc) => {
-      return res.status(200).send({ users: doc });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).send();
-    });
-});
-
-router.route("/admin/user/crud/:id").get((req, res) => {
-  const id = req.params.id;
-  if (id === undefined || id === "" || !Number.isInteger(+id))
-    return res.status(400).send({ message: "url id is not valid" });
-  User.findOne({ user_id: id })
-    .then((doc) => {
-      if (doc) return res.status(200).send(doc);
-      return res.status(404).send({ message: "post with this id not found" });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).send();
-    });
-});
-
-router.route("/signin").post(async (req, res) => {
-  const { email, password } = req.body;
-  if (Object.keys(req.body).length !== 2)
-    return res.status(400).send({ message: "Request Length should be 2" });
-  if (email === undefined || !validateEmail(email))
-    return res.status(400).send({ message: "filed `email` is not valid" });
-  if (password === undefined || password.length <= 5)
-    return res
-      .status(400)
-      .send({ message: "filed `password`.length should be gt 5" });
-
-  const user_same_email = await User.findOne({ email: email });
-  if (!user_same_email) {
-    return res.status(409).send({ message: "email already exist." });
-  }
-
-  const token = jwt.sign(
-    {
-      id: user_same_email.user_id,
-    },
-    "secret",
-    { expiresIn: "1h" }
-  );
-  console.log(token);
-  res.send({ token: token });
-});
-
-router.route("/check").get((req, res) => {
-  const token = req.body.token || "ali";
-  jwt.verify(token, "secret", (err, id) => {
-    if (err) res.status(400).send();
-    res.send(id);
-  });
-  res.send();
-});
 
 module.exports = router;
--- HW3/src/server.js
+++ HW3/src/server.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const router = require("./router");
+const router = require("./routers/router");
+const router_signin = require("./routers/router.sign")
+const router_user = require("./routers/router.user")
 const port = 4000;
 
 var uri = "mongodb://localhost:27017/details";
@@ -10,6 +12,8 @@ mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/", router);
+app.use("/api", router_signin)
+app.use("/api/admin/user", router_user)
 
 app.listen(port, function () {
   console.log("Server is running on Port: " + port);
